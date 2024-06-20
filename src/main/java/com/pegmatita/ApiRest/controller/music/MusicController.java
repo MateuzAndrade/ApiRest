ac1dcdd1bed76613f330631fd460c89319cebf0b
Mateus Andrade <mateus.nascimento.andrade@gmail.com>
2024-06-20 06:17:16 -0300

    Ajuste do service

--- src/main/java/com/pegmatita/ApiRest/controller/music/MusicController.java
+++ src/main/java/com/pegmatita/ApiRest/controller/music/MusicController.java
@@ -9,26 +9,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pegmatita.ApiRest.model.music.Artist;
-import com.pegmatita.ApiRest.model.music.Disc;
 import com.pegmatita.ApiRest.model.music.Music;
-import com.pegmatita.ApiRest.repository.music.ArtistRepository;
-import com.pegmatita.ApiRest.repository.music.DiscRepository;
 import com.pegmatita.ApiRest.repository.music.MusicRepository;
+import com.pegmatita.ApiRest.service.music.MusicService;
 
 @RestController
 @RequestMapping("/music")
 public class MusicController {
-
     
-     @Autowired
+    @Autowired
     private MusicRepository musicRepository;
 
     @Autowired
-    private DiscRepository discRepository;
-
-    @Autowired
-    private ArtistRepository artistRepository;
+    private MusicService musicService;
 
     @GetMapping("/list")
     public List<Music> listMusic(){
@@ -37,20 +30,7 @@ public class MusicController {
 
     @PostMapping("/create")
     public Music createMusic(@RequestBody Music music) {
-        // Verificar e salvar o Disc se ele ainda não estiver salvo
-        Disc disc = music.getDisc();
-        if (disc != null && disc.getId() == null) {
-            discRepository.save(disc);
-        }
-        
-        // Verificar e salvar o Artist se ele ainda não estiver salvo
-        Artist artist = music.getArtist();
-        if (artist != null && artist.getId() == null) {
-            artistRepository.save(artist);
-        }
-        
-        // Agora salvar a entidade Music
-        return musicRepository.save(music);
+        return musicService.createMusic(music);
     }
     
 }
--- src/main/java/com/pegmatita/ApiRest/service/music/MusicService.java
+++ src/main/java/com/pegmatita/ApiRest/service/music/MusicService.java
@@ -1,5 +1,47 @@
 package com.pegmatita.ApiRest.service.music;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.pegmatita.ApiRest.model.music.Artist;
+import com.pegmatita.ApiRest.model.music.Disc;
+import com.pegmatita.ApiRest.model.music.Music;
+import com.pegmatita.ApiRest.repository.music.ArtistRepository;
+import com.pegmatita.ApiRest.repository.music.DiscRepository;
+import com.pegmatita.ApiRest.repository.music.MusicRepository;
+
+import jakarta.transaction.Transactional;
+
 public class MusicService {
+
+    @Autowired
+    public MusicRepository musicRepository;
+
+    @Autowired
+    public DiscRepository discRepository;
+
+    @Autowired
+    public ArtistRepository artistRepository;
+
+    @Transactional
+    public Music createMusic(Music music) {
+        try {
+            Disc disc = music.getDisc();
+            if (disc != null && disc.getId() == null) {
+                discRepository.save(disc);
+            }
+
+            Artist artist = music.getArtist();
+            if (artist != null && artist.getId() == null) {
+                artistRepository.save(artist);
+            }
+
+            return musicRepository.save(music);
+
+
+        } catch (Exception e) {
+            System.err.println("Erro ao salvar a música: " + e.getMessage());
+            return null;
+        }
+    }
 
 }
